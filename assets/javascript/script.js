494feafb78eec4168d668018729634fb98f2b0d4
Lewis Fong <fongl@bu.edu>
2018-02-26 13:06:49 -0800

    added gJax and wJax functions, they request json from google and tugo

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,14 +15,17 @@ let articleUrl;
 let dataUrl;
 let countryUrl;
 let country;
+let globalInput;
+let googleOutput;
+let fullAddress;
 
 // Hide result divs on pageload, animate header and search button, run search function
 $(document).ready(function() {
-  $header.hide().fadeIn(2000);
+  $header.hide().fadeIn(250);
   $inputCard
     .hide()
-    .delay(1000)
-    .fadeIn(2000);
+    .delay(500)
+    .fadeIn(900);
   $alertCard.hide();
   $mapCard.hide();
   $newsCard.hide();
@@ -47,12 +50,13 @@ function input() {
       reset();
       $search.css("border-bottom", "2px solid rgb(9, 142, 14)");
       let input = $(this).val();
+      globalInput = input;
       $inputCard.delay(500).slideUp(1000);
       setTimeout(showCards, 1500);
-      console.log("[LOG] " + input);
+      //console.log("[LOG] " + input);
 
       //   Test appends
-      let pDiv = $('#alertDiv');
+      let pDiv = $("#alertDiv");
       pDiv.text(input);
       let $li = $("<li>");
       $li.text(input + " List Item(s)");
@@ -63,9 +67,75 @@ function input() {
         "https://www.google.com/maps/embed/v1/search?key=AIzaSyCv-DHBFYZNL-eaSZDKZRzE_BE5LpMcUe4&q=" +
         input;
       $("iframe").attr("src", mapUrl);
+
+      //call the google ajax function, which in turn calls wJax()
+      gJax();
     }
   });
 }
+
+//ajax call the google map api to get a country code which is used in wJax()
+function gJax() {
+  $.ajax({
+    url:
+      "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+      globalInput,
+    method: "GET"
+  }).then(function(response) {
+    var res = response.results;
+    $("#alertDiv").text(res[0].formatted_address);
+    for (var i = 0; i < res[0].address_components.length; i++) {
+      if (res[0].address_components[i].types[0] == "country") {
+        googleOutput = res[0].address_components[i].short_name;
+      }
+    }
+    wJax();
+  });
+}
+
+//pull and display travel warning based on the country code gJax() provides
+function wJax() {
+  console.log(googleOutput);
+  $.ajax({
+    url: "https://api.tugo.com/v1/travelsafe/countries/" + googleOutput,
+    headers: {
+      "X-Auth-API-Key": "kew824h7b2xpjnw9aadbrq6k"
+    },
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    displayWarning();
+
+    function displayWarning() {
+      
+      let advisoryDescription = $("<p>");
+      advisoryDescription.text(response.advisories.description);
+      let simpleAdvice = $("<p>");
+      if(response.advisoryState==0){
+        simpleAdvice.text("Advice: Proceed with normal precautions");
+      }
+      else if (response.advisoryState==1)
+      {
+        simpleAdvice.text("Advice: Excercise increased caution");
+      }
+      else if (response.advisoryState==2)
+      {
+        simpleAdvice.text("Advice: Reconsider destination");
+      }
+      else  (response.advisoryState==3)
+      {
+        simpleAdvice.text("Advice: Do not travel");
+      }
+      $("#alertCard").append(simpleAdvice);
+      $("#alertCard").append(advisoryDescription);
+
+
+    }
+
+  });
+}
+
+// Display County Warnings
 
 // Show result cards
 function showCards() {
@@ -77,26 +147,26 @@ function showCards() {
 
 // Reset page on button click
 function reset() {
-$button.on("click", function() {
-  //   Prevents dupicate click assignments
-  event.preventDefault();
-  $button.off("click");
+  $button.on("click", function() {
+    //   Prevents dupicate click assignments
+    event.preventDefault();
+    $button.off("click");
 
-  //   Resets result divs and removes them from page, reloads search div, re-runs input function
-  $search.val("");
-  $search.css("border-bottom", "2px solid rgb(255, 255, 255)");
-  $search.off("focus");
-  $('p').html('');
-  $("ul").html('')
-  $inputCard
-    .hide()
-    .delay(500)
-    .fadeIn(1000);
-  $alertCard.hide();
-  $mapCard.hide();
-  $newsCard.hide();
-  $button.hide();
-  $("select").material_select();
-  input();
-});
+    //   Resets result divs and removes them from page, reloads search div, re-runs input function
+    $search.val("");
+    $search.css("border-bottom", "2px solid rgb(255, 255, 255)");
+    $search.off("focus");
+    $("p").html("");
+    $("ul").html("");
+    $inputCard
+      .hide()
+      .delay(500)
+      .fadeIn(1000);
+    $alertCard.hide();
+    $mapCard.hide();
+    $newsCard.hide();
+    $button.hide();
+    $("select").material_select();
+    input();
+  });
 }
